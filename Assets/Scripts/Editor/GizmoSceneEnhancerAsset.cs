de155534cd180efde666f27c1801d633cd6ef2a3
Andrea <andrea.carpentier-21@orange.fr>
2021-02-09 08:15:58 +0100

    Initial commit

---
+++ Assets/Scripts/Editor/GizmoSceneEnhancerAsset.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace technical.test.editor
+{
+    [CreateAssetMenu(fileName = "Gizmo Scene Enhancer Asset", menuName = "Custom/Create Gizmo Scene Enhancer Asset")]
+    public class GizmoSceneEnhancerAsset : ScriptableObject
+    {
+        [SerializeField] private SceneGizmo[] _gizmos = default;
+
+        public override string ToString()
+        {
+            return "Gizmo count : " + _gizmos.Length;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Editor/SceneGizmo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace technical.test.editor
+{
+    [Serializable]
+    public struct SceneGizmo
+    {
+        public string Name;   
+        public Vector3 Position;
+
+        public SceneGizmo(string name, Vector3 position)
+        {
+            Name = name;
+            Position = position;
+        }
+    }
+
+}
